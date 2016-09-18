87d4c4ed966f5d247b36a5fd5e78599ac0c773a2
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-09-18 12:07:59 +0300

    Fix todayStudy script

--- scripts/todayStudy.sh
+++ scripts/todayStudy.sh
@@ -19,7 +19,7 @@ case $day in
         6) out="10:00 po-po-net-net 16:50" ;;
         7) out=$free ;;
 
-        8) out="10:00 pe-__-funcan-pp-pp 18:40" ;;
+        8) out="08:20 pe-11:40-ml-ml-func-func 18:30" ;;
         9) out="10:00 cod-cod-db 15:00" ;;
         10) out="no university" ;;
         11) out="11:40 seg-seg 15:40" ;;

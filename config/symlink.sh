9b9366fe32866438e1a7716a5124435a9a11c566
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-10-29 20:37:46 +0300

    Add stack config.yaml

--- config/symlink.sh
+++ config/symlink.sh
@@ -5,19 +5,21 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # $1 filename, $2 real path
 function makeLink {
   rm $2
-  ln -s $dir$1 $2
+  ln -s $dir/$1 $2
 }
 
-makeLink "/.xmobarrc" ~/.xmobarrc
-makeLink "/.vimperatorrc" ~/.vimperatorrc
-makeLink "/.xinitrc" ~/.xinitrc
-makeLink "/.gitconfig" ~/.gitconfig
-makeLink "/xmonad.hs" ~/.xmonad/xmonad.hs
-makeLink "/.emacs" ~/.emacs
-makeLink "/.xmodmap" ~/.xmodmap
-makeLink "/.zshrc" ~/.zshrc
-makeLink "/.mrconfig" ~/.mrconfig
+makeLink "./.xmobarrc" ~/.xmobarrc
+makeLink "./.vimperatorrc" ~/.vimperatorrc
+makeLink "./.xinitrc" ~/.xinitrc
+makeLink "./.gitconfig" ~/.gitconfig
+makeLink "./xmonad.hs" ~/.xmonad/xmonad.hs
+makeLink "./.emacs" ~/.emacs
+makeLink "./.xmodmap" ~/.xmodmap
+makeLink "./.zshrc" ~/.zshrc
+makeLink "./.mrconfig" ~/.mrconfig
 mkdir -p ~/.config/dunstrc/
-makeLink "/dunstrc" ~/.config/dunst/dunstrc
-makeLink "/.vimrc" ~/.vimrc
-makeLink "/.bashrc" ~/.bashrc
+makeLink "./dunstrc" ~/.config/dunst/dunstrc
+makeLink "./.vimrc" ~/.vimrc
+makeLink "./.bashrc" ~/.bashrc
+mkdir -p ~/.stack
+makeLink "./config.yaml" ~/.stack/config.yaml

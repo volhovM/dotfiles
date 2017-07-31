c1590d1ac2369255e386881f2d033a606b338587
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-07-31 11:35:09 +0300

    Consider charging state in lowBattery.sh

--- scripts/lowBattery.sh
+++ scripts/lowBattery.sh
@@ -1,8 +1,16 @@
 battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-if [ $battery_level -le 8 ]
-then
-    notify-send -u critical "Battery low" "Battery level is critical: ${battery_level}%!"
-elif [ $battery_level -le 15 ]
-then
-    notify-send "Battery low" "Battery level is ${battery_level}%!"
+discharging=`acpi -b | grep Discharging`
+if [ ! -z "$discharging" ]; then 
+  if [ $battery_level -le 4 ]
+  then
+      notify-send -u critical "TURNING OFF PC IN 5s (battery low)" "Battery level is SUPER LOW: ${battery_level}%!"
+      sleep 5
+      systemctl poweroff
+  elif [ $battery_level -le 8 ]
+  then
+      notify-send -u critical "Battery low" "Battery level is critical: ${battery_level}%!"
+  elif [ $battery_level -le 15 ]
+  then
+      notify-send "Battery low" "Battery level is ${battery_level}%!"
+  fi
 fi

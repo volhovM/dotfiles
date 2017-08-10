73f1afa5ef67852871807685578ede33dde214f8
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-08-10 19:05:29 +0300

    Fix workstats.sh

--- scripts/workstats.sh
+++ scripts/workstats.sh
@@ -3,4 +3,5 @@
 cd ~/code/serokell/yt-utilities
 ytToken=$(cat ~/.yttoken)
 lastMon=$(date -dlast-monday +%Y-%m-%d)
-./Export.hs --org ~/org/work.org --token "$ytToken" --user volhovm --dry-run --since $lastMon | tail -n 1
+echo $lastMon
+./Export.hs --org ~/org/work.org --token "$ytToken" --user volhovm --mode Local --since $lastMon | tail -n 1

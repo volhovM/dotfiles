e5ff0851660ca18c2290af9b0504793382b1821b
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-09-19 22:13:00 +0300

    Rewrite workstats.sh in lazy manner so xmobar doesn't lag

--- scripts/workstats.sh
+++ scripts/workstats.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+workstatsFile=~/.workstats
+yttokenFile=~/.yttoken
+
 cd ~/code/serokell/yt-utilities
-ytToken=$(cat ~/.yttoken)
+ytToken=$(cat $yttokenFile)
 lastMon=$(date -dlast-monday +%Y-%m-%d)
-./Export.hs --org ~/org/work.org --token "$ytToken" --user volhovm --mode Local --since $lastMon | tail -n 1
+cat $workstatsFile | head -n 1
+# Lazily write to file and read the old version instead. 
+# Xmobar doesn't like long actions and lags as hell.
+(./Export.hs --org ~/org/work.org --token "$ytToken" --user volhovm --mode Local --since $lastMon | tail -n 1 > $workstatsFile; echo "last updated on $(date)" >> $workstatsFile)&

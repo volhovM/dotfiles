a4d031ddd6716db04782892ea814d2c484f684bb
Mikhail Volkhov <volhovm.cs@gmail.com>
2019-03-12 20:47:18 +0100

    Improve expenses a bit

--- scripts/thisMonthExpenses.sh
+++ scripts/thisMonthExpenses.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-ledger -f ~/org/finances.dat b Expenses -X EUR --cost -p "this month" --collapse | tr '\n' ' ' | sed -E "s/Expenses//g" | xargs
+
+finFile=~/org/finances.dat
+
+cap=$(grep "%BUDGET%" $finFile | cut -d' ' -f3)
+
+amount=$(ledger -f $finFile b Expenses -X EUR --cost -p "this month" --collapse | tr '\n' ' ' | sed -E "s/Expenses//g;s/ EUR//g" | xargs)
+[ -z "$amount" ] && amount="0"
+days=$(cal $(date +"%m %Y") | awk 'NF {DAYS = $NF}; END {print DAYS}')
+curday=$(date +"%d")
+left1=$(python -c "print(str(round(($cap - $amount),2)))")
+left2=$(python -c "print(str(round(($left1/($days-$curday)), 2)))")
+echo "$left2 <fc=#355254>($left1)</fc>"

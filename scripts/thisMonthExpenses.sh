901b0e1c2486cb12b3ddd67ae615887155bccfad
Mikhail Volkhov <volhovm.cs@gmail.com>
2019-03-30 09:50:01 +0100

    fix thisMonthExpenses script

--- scripts/thisMonthExpenses.sh
+++ scripts/thisMonthExpenses.sh
@@ -8,6 +8,6 @@ amount=$(ledger -f $finFile b Expenses -X EUR --cost -p "this month" --collapse 
 [ -z "$amount" ] && amount="0"
 days=$(cal $(date +"%m %Y") | awk 'NF {DAYS = $NF}; END {print DAYS}')
 curday=$(date +"%d")
-left1=$(python -c "print(str(round(($cap - $amount),2)))")
-left2=$(python -c "print(str(round(($left1/($days-$curday)), 2)))")
-echo "$left2 <fc=#355254>($left1)</fc>"
+leftTotal=$(python -c "print(str(round(($cap - $amount),2)))")
+leftPerDay=$(python -c "print(str(round(($leftTotal/($days-$curday+1)), 2)))")
+echo "$leftPerDay <fc=#355254>($leftTotal)</fc>"

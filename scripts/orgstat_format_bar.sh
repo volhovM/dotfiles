ab148ae101dcb7e160a28d16ed6049847f685be5
Mikhail Volkhov <volhovm.cs@gmail.com>
2019-06-11 20:58:39 +0200

    Better color gradient

--- scripts/orgstat_format_bar.sh
+++ scripts/orgstat_format_bar.sh
@@ -1,9 +1,11 @@
 
-# TODO this is not finished, as this script called from orgstat doesn't see python
-
 # converts from a:b to a + b/60
 function convert_from {
   IFS=":" read a b <<< $(echo $1)
+
+  # strip leading 0s
+  if [ "$a" != "00" ]; then a=$(echo $a | sed 's/^0*//'); fi
+  if [ "$b" != "00" ]; then b=$(echo $b | sed 's/^0*//'); fi
 
   echo $( echo "print ($a + $b/60)" | python )
 }
@@ -11,15 +13,24 @@ function convert_from {
 # first 6 arguments are r1g1b1 r2g2b2, 7th and 8th arguments are limits, 9th is value
 function avg_col {
 
-  left=$7
-  right=$8
-  val=$(convert_from $9)
+  left=$1
+  right=$2
+  val=$3
 
-  k=$(echo "print( ($val - $left)/($right - $left) ) if $val <= $right else print(1)" | python)
+  k=$(echo "print(max(0, min(1,($val - $left)/($right - $left))) )" | python)
 
-  r=$(echo "import math; print (math.floor($1 + $k*($4 - $1)))" | python)
-  g=$(echo "import math; print (math.floor($2 + $k*($5 - $2)))" | python)
-  b=$(echo "import math; print (math.floor($3 + $k*($6 - $3)))" | python)
+
+  if python -c "exit(0 if $k<0.5 else 1)"; then
+    # gradient between red and yellow
+    r=255
+    g=$(echo "import math; print (math.floor(($k*2)*255))" | python)
+    b=0
+  else
+    # between yellow and green
+    r=$(echo "import math; print (255 - math.floor(($k-0.5)*2*255))" | python)
+    g=255
+    b=0
+  fi
 
   r=$(printf '%02x' $r)
   g=$(printf '%02x' $g)
@@ -37,9 +48,10 @@ sRateHigh=$(echo "print (40 * ($daynum/7))" | python)
 hRateHigh=$(echo "print (20 * ($daynum/7))" | python)
 eRateHigh=$(echo "print (20 * ($daynum/7))" | python)
 
-sColor=$(avg_col 255 0 0 0 255 0 0 $sRateHigh $thisWeekStudy)
-hColor=$(avg_col 255 0 0 0 255 0 0 $hRateHigh $thisWeekH)
-eColor=$(avg_col 0 255 0 255 0 0 0 $eRateHigh $thisWeekE)
+sColor=$(avg_col 0 $sRateHigh $(convert_from $thisWeekStudy) )
+hColor=$(avg_col 0 $hRateHigh $(convert_from $thisWeekH) )
+thisWeekENum=$(convert_from $thisWeekE)
+eColor=$(avg_col 0 $eRateHigh $(python -c "print ($eRateHigh - $thisWeekENum)") )
 
 str="<fc=$sColor>s$thisWeekStudy</fc> <fc=$hColor>h$thisWeekH</fc> <fc=$eColor>e$thisWeekE</fc> <fc=#355254>a$thisWeekA</fc>"
 

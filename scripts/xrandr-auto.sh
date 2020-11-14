a8a627736fed5b3b3ed6258b53a27ea9200a0500
Mikhail Volkhov <volhovm.cs@gmail.com>
2020-11-14 18:08:42 +0000

    Support multiple displays in xrandr script

--- scripts/xrandr-auto.sh
+++ scripts/xrandr-auto.sh
@@ -2,14 +2,22 @@
 
 # Generalise to other inputs later...
 #externalConnected=$(xrandr | grep -E "^(DP1 connected|DP2 connected|HDMI1 connected|HDMI2 connected)")
-externalConnected=$(xrandr | grep -E "^DP1 connected")
+#externalConnected=$(xrandr | grep -E "^DP1 connected")
+
+dType=""
+if xrandr | grep -E "^DP1 connected"; then dType="DP1"
+elif xrandr | grep -E "^HDMI1 connected"; then dType="HDMI1"
+elif xrandr | grep -E "^HDMI2 connected"; then dType="HDMI2"
+fi
+
+echo $dType
 
 xrandr --auto
 
-if [ -z "$externalConnected" ]; then
+if [ -z "$dType" ]; then
   xrandr --output eDP1 --primary --dpi 130
 else
-  xrandr --output DP1 --primary --above eDP1 --dpi 130
+  xrandr --output $dType --primary --above eDP1 --dpi 130
 fi
 
 feh --bg-fill ~/thisWeekTimeline.png
